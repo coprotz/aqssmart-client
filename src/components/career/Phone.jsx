c669ad9ddf9de95acc5c50594e82fcd08dee54c7
coprotz <qsallya@gmail.com>
2022-03-21 13:36:16 +0300

    helo

--- src/components/career/Phone.jsx
+++ src/components/career/Phone.jsx
@@ -32,7 +32,7 @@ const Phone = ({ go, next, previous, formData, setForm }) => {
 
     try {
       dispatch(signinUser(verifyPhone))
-      if(status !== 'rejected'){
+      if(signinStatus === 'success'){
         next();
       }
       
