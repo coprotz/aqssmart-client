0e808617047da29770a1bd91ca97d8b007f5a3f2
coprotz <qsallya@gmail.com>
2022-03-22 17:56:28 +0300

    forth

--- src/components/career/Phone.jsx
+++ src/components/career/Phone.jsx
@@ -19,12 +19,12 @@ const Phone = ({ go, next, previous, formData, setForm }) => {
 
   // console.log(signinStatus)
 
-  // const type = useSelector((state) => state?.authUser?.user?.type) 
-  // const message = useSelector((state) => state?.authUser?.user?.message)
-  const { type, message } = (state) => state?.users?.signin?.data
+  const type = useSelector((state) => state?.authUser?.user?.type) 
+  const message = useSelector((state) => state?.authUser?.user?.message)
+  // const { type, message } = (state) => state?.users?.signin?.data
 
-  // console.log('type',type)
-  // console.log('message', message)
+  console.log('type',type)
+  console.log('message', message)
 
   const Continue = async (e) => {
     e.preventDefault();
